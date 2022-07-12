d3bf0ec3b6166321861150931a37bffa47f41c65
JaimeRomero <jaime_im@outlook.com>
2022-07-11 21:08:25 -0500

    Se crearon las funciones manejarCambio y manejarEnvio

--- src/componentes/TareaFormulario.js
+++ src/componentes/TareaFormulario.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "../estilos/TareaFormulario.css"
 
 function TareaFormulario(props){
+   
+   const [input, setInput] = useState('');
+
+   const manejarCambio = e =>{
+        setInput(e.target.value);  
+   }
+
+   const manejarEnvio = e =>{
+      const tareaNueva = {
+        id: "34545",
+        texto: "Hola"
+      }
+   }
    return(
       <form className="tarea-formulario">
         <input
           className="tarea-input"
           type="text"
           placeholder="Escribe una tarea"
-          name="texto" 
+          name="texto"
+          onChange={manejarCambio} 
         />
         <button className="tarea-boton">Agregar Tarea</button>
 
